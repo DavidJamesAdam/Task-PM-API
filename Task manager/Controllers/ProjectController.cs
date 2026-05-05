7e0696da3e48d5ba2625f1a44868e10d3ebcc210
DavidJamesAdam <davidjamesadam@gmail.com>
2026-05-05 13:58:50 -0600

    Started on posting a project

--- Task manager/Controllers/ProjectController.cs
+++ Task manager/Controllers/ProjectController.cs
@@ -79,6 +79,8 @@ namespace Task_manager.Controllers
         [HttpPost]
         public async Task<ActionResult<Projects>> PostProjects(Projects projects)
         {
+            string? userInfo = Request.Cookies["UserIdentifier"];
+            Console.WriteLine(userInfo);
             _context.Projects.Add(projects);
             await _context.SaveChangesAsync();
 
